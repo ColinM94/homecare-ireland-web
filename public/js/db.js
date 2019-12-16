94dba48213b4f9a282230383f7165ca36be1e52a
Colin <colinmaher94@gmail.com>
2019-12-16 18:27:35 +0000

    Changes to clients page
    - Implement add client form.
    - Fix client datatable errors.

--- public/js/db.js
+++ public/js/db.js
@@ -48,31 +48,88 @@ function setupClients () {
 
             client[0] = doc.id
             client[1] = doc.data().name
-            client[2] = doc.data().address1
-            client[3] = doc.data().address2
-            client[4] = doc.data().town
-            client[5] = doc.data().county
-            client[6] = doc.data().eircode
-            client[7] = doc.data().mobile
+            client[2] = doc.data().mobile
+            client[3] = doc.data().address1
+            client[4] = doc.data().address2
+            client[5] = doc.data().town
+            client[6] = doc.data().county
+            client[7] = doc.data().eircode
             client[8] = doc.data().marital
 
             clients.push(client)
         })
 
-            $('#clients').DataTable( {
+            $('#clientsTable').DataTable( {
             data: clients,
             columns: [
                 { title: "ID" },
                 { title: "Name" },
+                { title: "Mobile"},
                 { title: "Address 1"},
                 { title: "Address 2"},
                 { title: "Town"},
                 { title: "County"},
                 { title: "Eircode"},
-                { title: "Mobile"},
                 { title: "Marital Status"},
-            ],
+                {mRender: function (data, type, row) {
+                    return `<a href="javascript:deleteClient('${row[0]}')">Delete</a>`
+                }},
+            ]
         })
     })
+}
+
+function addClient () {
+
+    var name = $("#client-name").val()
+    var mobile = $("#client-mobile").val()
+    var address1 = $("#client-address1").val()
+    var address2 = $("#client-address2").val()
+    var town = $("#client-town").val()
+    var county = $("#client-county").val()
+    var eircode = $("#client-eircode").val()
+    var marital = $("#client-marital").val()
+
+    log("Adding Client")
+
+    let client = {
+        name: name,
+        mobile: mobile
+    }
+
+    let clientDetails = {
+        name : name,
+        mobile : mobile,
+        address1 : address1,
+        address2 : address2 ,
+        town : town,
+        county : county,
+        eircode : eircode,
+        marital : marital
+    }
+
+    db.collection("clients").add(client).then(ref => {
+        log("Adding client with id", ref.id)
+        db.collection("clientDetails").doc(ref.id).set(clientDetails) 
+    }).catch(error => {
+        log(error.message)
+    })
+
+    return false
+}
+
+function deleteClient (clientId) {
+    db.collection('clients').doc(clientId).delete()
+    db.collection('clientDetails').doc(clientId).delete()
+    db.collection('connections').doc(clientId).delete()
+
+    var table = $('#clientsTable').DataTable()
+
+    table
+    .rows( function ( idx, data, node ) {
+        return data[0] === clientId;
+    } )
+    .remove()
+    .draw();
 }
 
